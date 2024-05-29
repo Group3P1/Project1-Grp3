07d945306897b9ca964a286fb421c3d6a358e225
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-05-28 22:15:53 -0500

    fetch for nutrition API

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,3 +7,12 @@ fetch("https://api.api-ninjas.com/v1/exercises?muscle=biceps",{
 }).then(function(data){
     console.log(data)
 })
+
+fetch("https://api.api-ninjas.com/",{
+    headers: { 'X-Api-Key': '6abbd1aa01c66ad0233cda2968c0b2ae'},
+    contentType: 'application/json',
+}).then(function(res){
+    return res.json()
+}).then(function(data){
+    console.log(data)
+})
