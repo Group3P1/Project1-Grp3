2847cef7f2bb76fe9290dc42e721444f3a136610
smrsun4 <smrsun4@gmail.com>
2024-05-30 19:05:24 -0400

    added input tag with id and JS for 2nd API for nutrition

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,31 @@
+const userResponse = document.getElementById("userResponse").value
+
 // const workEl = document.getElementById 
 fetch("https://api.api-ninjas.com/v1/exercises?muscle=biceps",{
     headers: { 'X-Api-Key': 'QZMGE5WHwNHEyx4FFqOkdg==NkZXuWGPrCiOnGz8'},
-    contentType: 'application/json',git 
+    contentType: 'application/json'
 }).then(function(res){
     return res.json()
 }).then(function(data){
     console.log(data)
 })
 
-fetch("https://",{
-    headers: { 'X-Api-Key': '6abbd1aa01c66ad0233cda2968c0b2ae'},
-    contentType: 'application/json',
-}).then(function(res){
-    return res.json()
-}).then(function(data){
-    console.log(data)
-})
+// fetch("https://trackapi.nutritionix.com/v2/search/instant/?query=hamburger",{
+//     headers: { 'X-Api-Key': '6abbd1aa01c66ad0233cda2968c0b2ae'},
+//     contentType: 'application/json',
+// }).then(function(res){
+//     return res.json()
+// }).then(function(data){
+//     console.log(data)
+// })
+
+
+var options = {
+  'method': 'GET',
+  'url': 'https://trackapi.nutritionix.com/v2/search/instant/?query=(userResponse)',
+  'headers': {
+    'Content-Type': 'application/json',
+    'x-app-id': 'AP ID faeec5b8',
+    'x-app-key': '6abbd1aa01c66ad0233cda2968c0b2ae'
+  }
+};
