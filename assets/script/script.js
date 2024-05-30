8967406d17e4eb451c171b2146b0d39acf6be6b1
smrsun4 <smrsun4@gmail.com>
2024-05-29 21:35:41 -0400

    put a fetch in JS to try out, waiting for API key

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,16 @@
 // const workEl = document.getElementById 
 fetch("https://api.api-ninjas.com/v1/exercises?muscle=biceps",{
     headers: { 'X-Api-Key': 'QZMGE5WHwNHEyx4FFqOkdg==NkZXuWGPrCiOnGz8'},
-    contentType: 'application/json',
+    contentType: 'application/json',git 
+}).then(function(res){
+    return res.json()
+}).then(function(data){
+    console.log(data)
+})
+
+fetch("https://www.nutritionix.com/api",{
+    headers: { 'X-Api-Key': ''},
+    contentType: 'application/json',git 
 }).then(function(res){
     return res.json()
 }).then(function(data){
